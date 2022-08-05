dafc27a7b0b325f46189e1002cca92843d04472e
saurabhshete100X <saurabhshete281@gmail.com>
2022-08-05 15:56:36 +0530

    nodejs intro

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
+router.get('/test-me', function (req, res) {    // route handler
     res.send('My first ever api!')
+});
+
+router.get('/test-you', function (req, res) {   // route handler
+    res.send('My second ever api!')
 });
 
 module.exports = router;
