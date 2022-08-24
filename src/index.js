4872c10276699c9b5c5355ba0a3e84a73334440a
saurabhshete100X <saurabhshete281@gmail.com>
2022-08-24 16:20:00 +0530

    Middleware 1 Assignment Done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const moment = require('moment');
+
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const { reset } = require('nodemon');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,15 +14,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
-app.use (
+
+app.use(
+
     function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+        let Date = moment().format('DD-MM-YYYY')
+        console.log(Date)
+        let ip = req.ip
+        let url = req.originalUrl
+        console.log(ip)
+        console.log(url)
+        return res.send({ Date: Date })
+    }
+);
+
+app.use(
+    function (req, res, next) {
+        console.log("inside GLOBAL MW");
         next();
-  }
-  );
+    }
+);
 
 app.use('/', route);
 
