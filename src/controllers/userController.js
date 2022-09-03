2b22b4748618bfea9daf58922d43afd3401228bf
saurabhshete100X <saurabhshete281@gmail.com>
2022-09-03 09:51:12 +0530

    Try Catch And Http Status Code Assignment Completed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,84 +1,93 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-/*
-  Read all the comments multiple times to understand why we are doing what we are doing in login api and getUserData api
-*/
 const createUser = async function (req, res) {
-  // You can name the req, res objects anything.
-  // but the first parameter is always the request 
-  // the second parameter is always the response
-  let data = req.body;
-  let savedData = await userModel.create(data);
-  console.log(res.newAtribute);
-  res.send({ msg: savedData });
+  try {
+    let data = req.body;
+    let savedData = await userModel.create(data);
+    console.log(res.newAtribute);
+    res.status(201).send({ msg: savedData });
+  }
+  catch (error) {
+    return res.status(500).send({ status: false, msg: "Server Error" });
+  }
 };
 
 const loginUser = async function (req, res) {
-  let userName = req.body.emailId;
-  let password = req.body.password;
-
-  let user = await userModel.findOne({ emailId: userName, password: password });
-  if (!user)
-    return res.send({
-      status: false,
-      msg: "username or the password is not corerct",
-    });
-
-  // Once the login is successful, create the jwt token with sign function
-  // Sign function has 2 inputs:
-  // Input 1 is the payload or the object containing data to be set in token
-  // The decision about what data to put in token depends on the business requirement
-  // Input 2 is the secret (This is basically a fixed value only set at the server. This value should be hard to guess)
-  // The same secret will be used to decode tokens 
-  let token = jwt.sign(
-    {
-      userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FunctionUp",
-    },
-    "functionup-plutonium-very-very-secret-key"
-  );
-  res.setHeader("x-auth-token", token);
-  res.send({ status: true, token: token });
+  try {
+    let userName = req.body.emailId;
+    let password = req.body.password;
+    let user = await userModel.findOne({ emailId: userName, password: password });
+    if (!user)
+      return res.status(401).send({
+        status: false,
+        msg: "username or the password is not correct",
+      });
+    // Once the login is successful, create the jwt token with sign function
+    // Sign function has 2 inputs:
+    // Input 1 is the payload or the object containing data to be set in token
+    // The decision about what data to put in token depends on the business requirement
+    // Input 2 is the secret (This is basically a fixed value only set at the server. This value should be hard to guess)
+    // The same secret will be used to decode tokens 
+    let token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        batch: "thorium",
+        organisation: "FunctionUp",
+      },
+      "functionup-plutonium-very-very-secret-key"
+    );
+    res.setHeader("x-auth-token", token);
+    res.status(201).send({ status: true, token: token });
+  }
+  catch (error) {
+    return res.status(500).send({ status: false, msg: "Server Error" })
+  }
 };
 
 const getUserData = async function (req, res) {
-
-
-  let userId = req.params.userId;
-  let userDetails = await userModel.findById(userId);
-  if (!userDetails)
-    return res.send({ status: false, msg: "No such user exists" });
-
-  res.send({ status: true, data: userDetails });
-  // Note: Try to see what happens if we change the secret while decoding the token
+  try {
+    let userId = req.params.userId;
+    console.log(userId)
+    let userDetails = await userModel.findById(userId);
+    if (!userDetails)
+      return res.status(404).send({ status: false, msg: "No such user exists" });
+    res.status(200).send({ status: true, data: userDetails });
+  }
+  catch (error) {
+    res.status(500).send({ status: false, msg: "Server Error" })
+  }
 };
 
 const updateUser = async function (req, res) {
-
-  let userId = req.params.userId;
-  let user = await userModel.findById(userId);
-  //Return an error if no user with the given id exists in the db
-  if (!user) {
-    return res.send("No such user exists");
+  try {
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(404).send("No such user exists");
+    }
+    let userData = req.body;
+    let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, { new: true });
+    res.status(201).send({ status: true, data: updatedUser });
   }
-
-  let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData,{new:true});
-  res.send({ status: true, data: updatedUser });
+  catch (error) {
+    res.status(500).send({ status: false, msg: "Server Error" })
+  }
 };
 
 const DeleteUser = async function (req, res) {
- 
-
-  let userId = req.params.userId
-  let user = await userModel.findById(userId);
-  if (!user) {
-    return res.send("No Such User Exists")
+  try {
+    let userId = req.params.userId
+    let user = await userModel.findById(userId);
+    if (!user) {
+      return res.send("No Such User Exists")
+    }
+    let DeleteUsers = await userModel.findOneAndUpdate({ _id: userId }, [{ $set: { isDeleted: true } }], { new: true });
+    res.send({ status: true, data: DeleteUsers })
   }
-  let DeleteUsers = await userModel.findOneAndUpdate({_id: userId},[{$set: {isDeleted : true}}],{new:true});                                           
-  res.send({ status: true, data: DeleteUsers })
+  catch (error) {
+    res.status(500).send({ status: false, msg: "Server Error" })
+  }
 }
 
 module.exports.createUser = createUser;
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,24 +1,35 @@
+const { response } = require("express");
 const jwt = require("jsonwebtoken")
 
 const authentication = async function (req, res, next) {
-    let token = req.headers["x-Auth-token"];
-    if (!token) token = req.headers["x-auth-token"];
-    if (!token) return res.send({ status: false, msg: "token must be present" });
-    console.log(token);
-    let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
-    if (!decodedToken)
-        return res.send({ status: false, msg: "token is invalid" });
-    next()
+    try {
+        let token = req.headers["x-auth-token"];
+        if (!token)
+            if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
+        console.log(token);
+        let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key");
+        if (!decodedToken)
+            return res.status(401).send({ status: false, msg: "token is invalid" });
+        next()
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, msg: "Server Error" });
+    }
 };
 
 const authorization = async function (req, res, next) {
-    let token = req.headers["x-auth-token"]
-    let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key")
-    let userToBeModified = req.params.userId
-    let userLoggedIn = decodedToken.userId
-    if (!decodedToken) return res.send({ status: false, msg: "token in Valid" })
-    if (userToBeModified != userLoggedIn) return res.send({ status: false, msg: "You Are LoggedIn But Not Authorised For This Activity" })
-    next()
+    try {
+        let token = req.headers["x-auth-token"]
+        let decodedToken = jwt.verify(token, "functionup-plutonium-very-very-secret-key")
+        let userToBeModified = req.params.userId
+        let userLoggedIn = decodedToken.userId
+        if (!decodedToken) return res.send({ status: false, msg: "token in Valid" })
+        if (userToBeModified != userLoggedIn) return res.status(403).send({ status: false, msg: "You Are LoggedIn But Not Authorised For This Activity" })
+        next()
+    }
+    catch {
+        res.send(500)({ status: false, msg: "Server Error" })
+    }
 }
 
 module.exports.authentication = authentication
